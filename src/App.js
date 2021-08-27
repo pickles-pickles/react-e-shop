05f136a9af95ae9386141741d447805f3066e483
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2021-08-27 16:29:42 +0300

    Context commented out. Console.logs removed

--- src/App.js
+++ src/App.js
@@ -1,65 +1,70 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/App.css';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Cart from './components/Cart';
-import Products from './components/Products';
-import Details from './components/Details';
-import Modal from './components/Modal';
-import { ProductProvider } from './context';
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/App.css'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Cart from './components/Cart'
+import Products from './components/Products'
+import Details from './components/Details'
+import Modal from './components/Modal'
+//import { ProductProvider } from './context'
 
+function App () {
+  const [cartItems, setCartItems] = useState([])
 
-function App() {
+  const [details, setDetails] = useState({})
+  const showDetails = product => {
+    setDetails(product)
+  }
 
-  const [cartItems, setCartItems] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalProduct, setModalProduct] = useState({})
 
-  const [details, setDetails] = useState({});
-  const showDetails = (product) =>{
-    setDetails(
-        product
-    )
-};
+  const handleAdd = product => {
+    setCartItems([...cartItems, { ...product }])
+  }
 
-const [isModalOpen, setIsModalOpen] = useState(false);
-const [modalProduct, setModalProduct] = useState({});
-
-  const handleAdd = (product) => {
-    setCartItems([...cartItems, { ...product },]);
-  };
-
-  const handleModalAdd = (product) => {
-    setModalProduct(product);
-    console.log(product);
-  };
+  const handleModalAdd = product => {
+    setModalProduct(product)
+  }
 
   return (
     <React.Fragment>
-      <ProductProvider>
-        <Router>
-          <Header />
+      {/*    <ProductProvider> */}
+      <Router>
+        <Header />
 
-          <Switch>
-            <Route exact path="/">
-              <Products handleAdd={(product) => handleAdd(product)}
-                        showDetails={(product) => showDetails(product)}
-                        setIsModalOpen={setIsModalOpen}
-                        modalProduct={modalProduct} handleModalAdd={(product) => handleModalAdd(product)} />
-            </Route>
-            <Route path="/cart">
-              <Cart setCartItems={setCartItems} cartItems={cartItems} />
-              </Route>
-              <Route path="/details">
-              <Details details={details} handleAdd={(details) => handleAdd(details)} handleModalAdd={(details) => handleModalAdd(details)}
-               setIsModalOpen={setIsModalOpen}/>
-              </Route>
-          </Switch>
-              <Modal isModalOpen={isModalOpen} modalProduct={modalProduct} setIsModalOpen={setIsModalOpen} />
-        </Router>
-      </ProductProvider>
-
+        <Switch>
+          <Route exact path='/'>
+            <Products
+              handleAdd={product => handleAdd(product)}
+              showDetails={product => showDetails(product)}
+              setIsModalOpen={setIsModalOpen}
+              modalProduct={modalProduct}
+              handleModalAdd={product => handleModalAdd(product)}
+            />
+          </Route>
+          <Route path='/cart'>
+            <Cart setCartItems={setCartItems} cartItems={cartItems} />
+          </Route>
+          <Route path='/details'>
+            <Details
+              details={details}
+              handleAdd={details => handleAdd(details)}
+              handleModalAdd={details => handleModalAdd(details)}
+              setIsModalOpen={setIsModalOpen}
+            />
+          </Route>
+        </Switch>
+        <Modal
+          isModalOpen={isModalOpen}
+          modalProduct={modalProduct}
+          setIsModalOpen={setIsModalOpen}
+        />
+      </Router>
+      {/*  </ProductProvider> */}
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/context-for-future-use/context.js
@@ -0,0 +1,19 @@
+/* import React, { useState } from 'react'
+import storeProducts from './data'
+
+export const ProductContext = React.createContext()
+export const ProductProvider = props => {
+  const [state] = useState({
+    products: storeProducts
+  })
+
+  return (
+    <ProductContext.Provider
+      value={{
+        ...state
+      }}
+    >
+      {props.children}
+    </ProductContext.Provider>
+  )
+} */
